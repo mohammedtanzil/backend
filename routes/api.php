3a3e8374dd8e9efc73932edfc0f5e561b7f8ab5c
tanzildewon <itanzil73@gmail.com>
2021-11-19 22:24:15 +0600

    check someting

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/sendmail',[ContactControllers::class,'sendemail']);
+Route::get('/test',function(){
+    return 'nice test';
+});
